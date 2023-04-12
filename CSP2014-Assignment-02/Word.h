f91866d93b5fefc7628e319048be2d231e7a75f1
James Boyd <88956638+jamesramsayboyd@users.noreply.github.com>
2023-04-12 21:25:49 +0800

    Refactoring Word class to have setters/getters

--- CSP2014-Assignment-02/Word.h
+++ CSP2014-Assignment-02/Word.h
@@ -2,11 +2,57 @@
 
 #include <string>
 
-/* James Boyd, Student ID: 10629572, 7/3/2023
+/* James Boyd, Student ID: 10629572, 12/4/2023
 A structure to store the details of a word from a dictionary file provided 
 by the user. The structure has three properties, all of type String: name, type
 and definition. There are no methods. */
-struct Word
+//struct Word
+//{
+//	std::string name, type, definition;
+//};
+
+class Word
 {
 	std::string name, type, definition;
+
+	// Default constructor
+	Word() {}
+
+	// Overloaded constructor allowing name, type and definition properties to be set
+	Word(string nameToSet, string typeToSet, string definitionToSet) 
+	{
+		// set properties
+	}
+	
+
+	// Setters and Getters
+	private: string getName()
+	{
+		return name;
+	}
+
+	private: void setName(string nameToSet)
+	{
+		this->name = nameToSet;
+	}
+
+	private: string getType()
+	{
+		return type;
+	}
+
+	private: void setType(string typeToSet)
+	{
+		this->type = typeToSet;
+	}
+
+	private: string getDefinition() 
+	{
+		return definition;
+	}
+
+	private: string setDefinition(string definitionToSet)
+	{
+		this->definition = definitionToSet;
+	}
 };
